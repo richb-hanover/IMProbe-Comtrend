22d84abda494893334f7151587dd720daf8dc466
Rich Brown <richb.hanover@gmail.com>
2017-03-19 22:14:58 -0400

    properly parse SNR values from HTML response.

--- im_comtrend.py
+++ im_comtrend.py
@@ -6,56 +6,32 @@
 import os
 import sys
 import getopt
+import urllib2
+import base64
+import re
 
 try:
     searchString = ""
     opts, args = getopt.getopt(sys.argv[1:], "")
 except getopt.GetoptError, err:
     searchString = "getopt error %d" % (err)
-# if args[0] == "$numericParam":          # check to see if the argument is the name of the parameter
-#     number = 0                          # if so, set it to a good initial value
-# else:
-#     number = int(args[0])               # otherwise, convert the string to an integer
-
-# Read the stdin file which contains the search String
-# f = sys.stdin # open stdin
-# searchString = f.readline().strip()     # get the line & remove leading & trailing whitespace
-# if (searchString == "$SearchString"):   # check to see if the value is the name of the parameter
-#     searchString = ""                   # if so, set it to a good initial value
-
-# Now to the work of the probe
-# searchString += "a"                     # add another "a" to the end of the string
-# number += 1                             # increment the number as well
-# retstring = "Hunky Dory!"               # Return string
-
-import urllib2
-import base64
-import re
 
 request = urllib2.Request('http://192.168.1.1/statsadsl.html')
 base64string = base64.b64encode('%s:%s' % ('root', '12345'))
 request.add_header("Authorization", "Basic %s" % base64string)
 result = urllib2.urlopen(request)
-ds = result
-us = "not yet"
 
 content = result.read()
-lines = content.split('</tr>')
+lines = content.split('</tr>')      # split on new <tr> elements
 
 lines = [elem for elem in lines if 'SNR' in elem ]
-print "Lines was: " + lines[0]
+line = lines[0]                     # get the first line with "SNR"
+regex = re.compile(r'\d+')
+p = regex.findall(line)             # isolate numbers ("0.1 dB 119 156" => ['0', '1', '119', '156'])
+# print "DS: %s, US: %s" % (p[2], p[3])
 
-THIS NEXT LINE IS BROKEN
-regex = r".*:([0-9]?)<.*([0-9]?)"
-match = re.search(regex, lines[0])
-print match
-print match.group(1)
-
-print "DS: %s, US: %s" % (match.group(1), match.group(2))
-
-retstring = "" # "'*****'.join(lines)
-
+retstring = ""
 retcode=0                               # probe (system) exit code
 
-print "\{ $ds := '%s', $us := '%s' } %s" % (ds, us, retstring)
+print "\{ $ds := '%s', $us := '%s' } %s" % (p[2], p[3], retstring)
 sys.exit(retcode)
