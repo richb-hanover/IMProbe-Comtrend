addebf218e167648d4fd1a64b64c09754cef9932
Rich Brown <richb.hanover@gmail.com>
2017-03-22 13:41:26 -0400

    Added timeout logic on URL connection

--- im_comtrend.py
+++ im_comtrend.py
@@ -9,6 +9,7 @@ import getopt
 import urllib2
 import base64
 import re
+import socket
 
 try:
     searchString = ""
@@ -19,19 +20,29 @@ except getopt.GetoptError, err:
 address = args[0]
 # print "Address: %s" % (address)
 
-# # Read the stdin file which contains the search String
-f = sys.stdin # open stdin
-credentials = f.readline().strip()     # get the line & remove leading & trailing whitespace
-# print "credentials: %s" % (credentials)
-#
-userpw = credentials.split(" ")
-user = userpw[0]
-password = userpw[1]
+if len(args) == 3:                          # if user & password args are on command line
+    user = args[1]                          # (supplied when testing)
+    password = args[2]
+else:
+    #                                       # Read the stdin file which contains the search String
+    f = sys.stdin                           # open stdin
+    credentials = f.readline().strip()      # get the line & remove leading & trailing whitespace
+    # print "credentials: %s" % (credentials)
+    #
+    userpw = credentials.split(" ")
+    user = userpw[0]
+    password = userpw[1]
 
-request = urllib2.Request('http://%s/statsadsl.html' % (address))
+request = urllib2.Request('http://%s/statsadsl.html' % address)
 base64string = base64.b64encode('%s:%s' % (user, password))
 request.add_header("Authorization", "Basic %s" % base64string)
-result = urllib2.urlopen(request)
+try:
+    result = urllib2.urlopen(request, timeout=3)
+except urllib2.URLError, e:
+    # For Python 2.7
+    # raise MyException("There was an error: %r" % e)
+    print "\{ } No response"        # No response - timed out
+    sys.exit(4)                     # return "Down" exit status
 
 content = result.read()
 lines = content.split('</tr>')      # split on new <tr> elements
