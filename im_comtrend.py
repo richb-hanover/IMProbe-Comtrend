0fca6940f9279a71ef33999aa78287d312f0d727
Rich Brown <richb.hanover@gmail.com>
2017-03-23 09:49:45 -0400

    Retrieve and display uptimes for modem, DSL, and pppoe link

--- im_comtrend.py
+++ im_comtrend.py
@@ -12,17 +12,7 @@ import re
 import socket
 
 '''
-scanForValues - retrieve down and upstream values from the named line
-'''
-def scanForValues(name, lines):
-    data = [elem for elem in lines if name in elem ]
-    line = data[0]                     # get the first line with "str"
-    regex = re.compile(r'\d+')
-    p = regex.findall(line)             # isolate numbers ("0.1 dB 119 156" => ['0', '1', '119', '156'])
-    return p[-2:]
-
-'''
-retrievePage() - get page from modem, break into lines at "</tr>"
+retrievePage() - get page from modem, handle errors
 '''
 def retrievePage(adrs, page, user, password):
     request = urllib2.Request('http://%s/%s' % (adrs,page))
@@ -35,9 +25,36 @@ def retrievePage(adrs, page, user, password):
         # raise MyException("There was an error: %r" % e)
         print "\{ } No response"  # No response - timed out
         sys.exit(4)  # return "Down" exit status
-    content = result.read()
-    return content.split('</tr>')  # split on new <tr> elements
+    return result.read()
 
+'''
+scanForValues - scan the lines and return down and upstream values from the named line
+'''
+def scanForValues(name, lines):
+    data = [elem for elem in lines if name in elem ]
+    line = data[0]                     # get the first line with "str"
+    regex = re.compile(r'\d+')
+    p = regex.findall(line)             # isolate numbers ("0.1 dB 119 156" => ['0', '1', '119', '156'])
+    return p[-2:]
+
+'''
+scanForTimes - scan the page and return the modem's uptime; the DSL uptime, and the pppoe uptime as strings
+'''
+def scanForTimes(page):
+    lines = page.split('</td>')                                 # split on </td> elements
+    data = [elem for elem in lines if '<td>' in elem]           # only keep lines with <td>
+    result = []
+    for str in data:                                              # only keep tail (with d:h:m:s values)
+        s = str.replace(" days:", "d, ")
+        s = s.replace(" hours:","h, ")
+        s = s.replace(" mins:","m, ")
+        s = s.replace(" secs","s")
+        result.append(re.sub(r'.*<td>', '', s.replace('\n','')))
+    return result[0:3]                                          # return first three entities
+
+'''
+Main Routine - parse arguments, get data from modem, format the results
+'''
 try:
     searchString = ""
     opts, args = getopt.getopt(sys.argv[1:], "")
@@ -60,16 +77,21 @@ else:
     user = userpw[0]
     password = userpw[1]
 
-lines = retrievePage(address, "statsadsl.html", user, password)
-
+# Retrieve SNR, Power, Attenuation values
+page = retrievePage(address, "statsadsl.html", user, password)
+lines = page.split('</tr>')  # split on new <tr> elements
 dSNR, uSNR = scanForValues("SNR", lines)
 dAtten, uAtten = scanForValues("Attenuation", lines)
 dPower, uPower = scanForValues("Output Power", lines)
 
+# Retrieve uptime values
+page = retrievePage(address, "showuptime.html", user, password)
+times = scanForTimes(page)                 # scan off the Uptime, DSL uptime, pppoe uptime
+
 retstring = ""
 retcode=0                               # probe (system) exit code
 
-print "\{ $dSNR := %s, $uSNR := %s, $dAtten := %s, $uAtten := %s, $dPower := %s, $uPower := %s }" \
-      % (dSNR, uSNR, dAtten, uAtten, dPower, uPower )
+print "\{ $dSNR := %s, $uSNR := %s, $dAtten := %s, $uAtten := %s, $dPower := %s, $uPower := %s, $up := '%s', $dslUp := '%s', $pppUp := '%s' }" \
+      % (dSNR, uSNR, dAtten, uAtten, dPower, uPower, times[0], times[1], times[2] )
 sys.exit(retcode)
 
