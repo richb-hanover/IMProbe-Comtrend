7ff868cab5b036d801c44ef9366645ca5b968692
Rich Brown <richb.hanover@gmail.com>
2017-09-27 18:38:29 -0400

    Final editorial tweaks before publishing

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # Intermapper probe for Comtrend DSL modem
 
-I wanted to keep track of the Signal to Noise Ratio (SNR) for my DSL modem. I was losing connections overnight, and wanted to prove to my ISP that this was happening.
+I wanted to keep track of the Signal to Noise Ratio (SNR) for my DSL modem. 
+I was losing connections overnight, and wanted to prove to my ISP that this was happening. 
+
+Intermapper now monitors and plots the SNR continually, so it's easy to see dips in the signal quality, and the times they occur. 
+This helped my ISP figure out what was happening.
+
+## The Probe
+
+This probe demonstrates a detailed InterMapper command-line probe, using the IMDC Python interpreter.
 
 This probe connects to, and scrapes the web interface of a Comtrend DSL modem to retrieve its SNR values and other important operational characteristics. 
 This has been tested with a Comtrend AR-5381U modem.
@@ -8,14 +16,13 @@ Here's the Status Window:
 
 ![Probe for Comtrend Modem](https://github.com/richb-hanover/IMProbe-Comtrend/blob/master/ComtrendModem.png?raw=true)
 
+To use the probe, use **File->Import Probe** and select the probe file `com.blueberryhillsoftware.comtrend.txt`
+
+To edit the probe, make changes to the `im_comtrend.py` file then copy/paste its contents into the `<tool> ... </tool>` section of the `com.blueberryhillsoftware.comtrend.txt` probe file and re-import it.
+
 See the full documentation at: [https://github.com/richb-hanover/IMProbe-Comtrend](https://github.com/richb-hanover/IMProbe-Comtrend)
-
-
-## About this probe
-
-Demonstrates a detailed InterMapper command-line probe, using the IMDC Python interpreter.
 
 --------
 Copyright (c) 2017 - Rich Brown, Blueberry Hill Software, [http://blueberryhillsoftware.com](http://blueberryhillsoftware.com)
 
-MIT License - See the footer of the file for the full statement
+MIT License - See the LICENSE file for the full statement
--- im_comtrend.py
+++ im_comtrend.py
@@ -7,14 +7,14 @@
 # and other important operational characteristics.
 # Tested with Comtrend AR-5381U
 #
-# See the full documentation at: github
+# See the full documentation at: https://github.com/richb-hanover/IMProbe-Comtrend
 #
 # Demonstrates a detailed Command-line probe, using the IMDC Python interpreter,
 # as well as the formatting available for a Status Window.
 #
 # ---------------------------------------------
 # Copyright (c) 2017 - Rich Brown, Blueberry Hill Software, http://blueberryhillsoftware.com
-# MIT License - See the footer of the file for the full statement
+# MIT License - See LICENSE file for the full statement
 
 import os
 import sys
@@ -127,24 +127,3 @@ retcode=0                               # probe (system) exit code
 print "\{ $dSNR := %s, $uSNR := %s, $dAtten := %s, $uAtten := %s, $dPower := %s, $uPower := %s, $mdmUp := '%s', $mdmS := '%s', $dslUp := '%s', $dslS := '%s', $pppUp := '%s', $pppS := '%s' }" \
       % (dSNR, uSNR, dAtten, uAtten, dPower, uPower, times[0], times[1], times[2], times[3], times[4], times[5] )
 sys.exit(retcode)
-
-# ---------------------------
-# MIT License
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
